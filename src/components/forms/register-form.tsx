a883d813d80371187f42d04864a44ead8b43a811
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2023-03-31 22:30:53 +0200

    more validation and password strength checker progress bar

--- src/components/forms/register-form.tsx
+++ src/components/forms/register-form.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { passwordStrength } from "check-password-strength";
 import { floatingLabels } from "../../helpers";
 import "./register-form.css";
+import { CSSTransition } from "react-transition-group";
 
-interface FormInputs {
+interface FormInputsInterface {
   email: string;
   password: string;
 }
@@ -15,36 +16,57 @@ const RegisterForm: React.FC = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<FormInputs>();
+  } = useForm<FormInputsInterface>();
   const [passStrengthCheck, setPassStrengthCheck] = useState<boolean>(false);
+  const fillRef = useRef<HTMLDivElement>(null);
+  const [passCheck, setPassCheck] = useState<boolean>(false);
+  const nodeRef = useRef(null);
 
   const emailOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     floatingLabels(event, ".register-form__email-input-label");
   };
 
+  const passLoseFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "") {
+      setPassCheck(false);
+    }
+    return;
+  };
+
   const passOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     floatingLabels(event, ".register-form__pass-input-label");
 
-    switch (passwordStrength(event.target.value).value) {
-      case "Too weak":
-        console.log("za slabe");
-        console.log(passwordStrength(event.target.value));
-        break;
-      case "Weak":
-        console.log("slabe");
-        console.log(passwordStrength(event.target.value));
-        break;
-      case "Medium":
-        console.log("sredniawka");
-        console.log(passwordStrength(event.target.value));
-        break;
-      case "Strong":
-        console.log("dobre");
-        console.log(passwordStrength(event.target.value));
-        break;
-      default:
-        console.log("default");
-        break;
+    if (fillRef.current && event.target.value === "") {
+      setPassCheck(false);
+      return;
+    }
+
+    if (event.target.value.length >= 1) {
+      setPassCheck(true);
+    }
+
+    if (fillRef.current) {
+      switch (passwordStrength(event.target.value).value) {
+        case "Too weak":
+          fillRef.current.style.width = "25%";
+          fillRef.current.style.backgroundColor = "#ff444f";
+          break;
+        case "Weak":
+          fillRef.current.style.width = "50%";
+          fillRef.current.style.backgroundColor = "#ffc107";
+          break;
+        case "Medium":
+          fillRef.current.style.width = "75%";
+          fillRef.current.style.backgroundColor = "#17a2b8";
+          break;
+        case "Strong":
+          fillRef.current.style.width = "100%";
+          fillRef.current.style.backgroundColor = "#28a745";
+          break;
+        default:
+          console.log("default");
+          break;
+      }
     }
   };
 
@@ -59,6 +81,7 @@ const RegisterForm: React.FC = () => {
           <input
             className="register-form__email-input"
             {...register("email", {
+              required: true,
               pattern:
                 /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
             })}
@@ -82,10 +105,25 @@ const RegisterForm: React.FC = () => {
             })}
             type="password"
             onChange={passOnChange}
+            onFocus={() => setPassCheck(true)}
+            onBlur={passLoseFocus}
           />
           <span className="register-form__pass-input-label">Password</span>
         </div>
-        {!passStrengthCheck && <div className="pass-str-check-container"></div>}
+        <CSSTransition
+          in={passCheck}
+          nodeRef={nodeRef}
+          classNames="pass-str-check-container"
+          timeout={500}
+          unmountOnExit
+          onEnter={() => setPassCheck(true)}
+          onExited={() => setPassCheck(false)}
+        >
+          <div ref={nodeRef} className="pass-str-check-container">
+            <div ref={fillRef} className="pass-str-check-container-filling" />
+          </div>
+        </CSSTransition>
+
         {errors.email && <span>Wrong email format</span>}
         {errors.password && (
           <span>Passwords needs to have more than 8 characters</span>
