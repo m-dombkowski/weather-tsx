cb760fe1dc4649947b4c9ad7e0fab862f2b8bcdf
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2023-10-22 21:44:39 +0200

    moved functions to different files

---
+++ src/services/supabase/api.ts
@@ -0,0 +1,74 @@
+import { User, createClient } from "@supabase/supabase-js";
+import { CityForecastInterface } from "../../state";
+import { triggerErrMessage } from "../../components/selected-city";
+import { supabase } from ".";
+
+export const addCityToFavs = async (
+  userData: User | undefined,
+  cityData: CityForecastInterface | undefined
+) => {
+  const { data, error } = await supabase
+    .from("favs")
+    .insert([
+      {
+        user: userData?.email,
+        city_data: cityData,
+        city_id: cityData?.city.id,
+      },
+    ])
+    .select();
+};
+
+export const removeCityFromFavs = async (
+  userData: User | undefined,
+  cityData: CityForecastInterface | undefined
+) => {
+  const { data: favs } = await supabase.from("favs").select("user,city_id");
+
+  const isAdded = favs?.filter(
+    (el) => el.user == userData?.email && el.city_id == cityData?.city.id
+  );
+  if (isAdded) {
+    const { error } = await supabase
+      .from("favs")
+      .delete()
+      .eq("city_id", isAdded[0].city_id as string);
+  }
+};
+
+export const checkIfCityIsAlreadyInFavs = async (
+  userData: User | undefined,
+  cityData: CityForecastInterface | undefined,
+  setFavError: React.Dispatch<React.SetStateAction<string>>,
+  ref: React.MutableRefObject<HTMLDivElement | null>
+) => {
+  const { data: favs, error } = await supabase
+    .from("favs")
+    .select("user,city_id");
+
+  const isAdded = favs?.find(
+    (el) => el.user == userData?.email && el.city_id == cityData?.city.id
+  );
+
+  if (!isAdded) {
+    return false;
+  }
+  triggerErrMessage(
+    setFavError,
+    ref,
+    "This city is already on your favorites list"
+  );
+  return true;
+};
+
+export const getUserCities = async (userData: User | undefined) => {
+  const { data: favs, error } = await supabase
+    .from("favs")
+    .select("user,city_id");
+
+  console.log(
+    favs?.filter((el) => {
+      el.user === userData?.email;
+    })
+  );
+};
--- src/services/supabase/index.ts
+++ src/services/supabase/index.ts
@@ -1,6 +1,4 @@
-import { User, createClient } from "@supabase/supabase-js";
-import { CityForecastInterface } from "../../state";
-import { triggerErrMessage } from "../../components/selected-city";
+import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 
@@ -11,77 +9,3 @@ export const supabase = createClient(
   supabaseUrl as string,
   supabaseApi as string
 );
-
-export const addCityToFavs = async (
-  userData: User | undefined,
-  cityData: CityForecastInterface | undefined
-) => {
-  const { data, error } = await supabase
-    .from("favs")
-    .insert([
-      {
-        user: userData?.email,
-        city_data: cityData,
-        city_id: cityData?.city.id,
-      },
-    ])
-    .select();
-};
-
-export const removeCityFromFavs = async (
-  userData: User | undefined,
-  cityData: CityForecastInterface | undefined
-) => {
-  const { data: favs } = await supabase.from("favs").select("user,city_id");
-
-  const isAdded = favs?.filter(
-    (el) => el.user == userData?.email && el.city_id == cityData?.city.id
-  );
-  if (isAdded) {
-    const { error } = await supabase
-      .from("favs")
-      .delete()
-      .eq("city_id", isAdded[0].city_id as string);
-  }
-};
-
-export const checkIfCityIsAlreadyInFavs = async (
-  userData: User | undefined,
-  cityData: CityForecastInterface | undefined,
-  setFavError: React.Dispatch<React.SetStateAction<string>>,
-  ref: React.MutableRefObject<HTMLDivElement | null>
-) => {
-  const { data: favs, error } = await supabase
-    .from("favs")
-    .select("user,city_id");
-
-  const isAdded = favs?.find(
-    (el) => el.user == userData?.email && el.city_id == cityData?.city.id
-  );
-
-  if (!isAdded) {
-    return false;
-  }
-  triggerErrMessage(
-    setFavError,
-    ref,
-    "This city is already on your favorites list"
-  );
-  return true;
-};
-
-export const getUserCities = async (
-  userData: User | undefined,
-  setCities: React.Dispatch<React.SetStateAction<never[]>>
-) => {
-  const { data: favs, error } = await supabase
-    .from("favs")
-    .select("user,city_id");
-
-  const cities = favs?.filter((el) => {
-    el.user === userData?.email;
-  });
-
-  setCities(cities);
-  return cities;
-};
